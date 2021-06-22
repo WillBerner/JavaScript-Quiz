eb21ab7b1d5c34d1067971b1471bd4e6cd6a6621
Will Berner <will.o.berner@gmail.com>
2021-06-22 03:02:42 -0400

    Created separate file for questions and answers. Started on quiz logic.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,64 @@
-var questionEl = document.querySelector(".question");
+import { questionArray } from "./questions.js"
+
 var timeEl = document.querySelector(".timer")
 
 var timeRemaining = 10;
 
-// Timer function
-var countdown = setInterval(function() {
-    if (timeRemaining > 1) {
-        timeEl.textContent = `${timeRemaining} seconds remaing`;
-        timeRemaining--;
-    } else if (timeRemaining === 1) {
-        timeEl.textContent = `${timeRemaining} seconds remaing`;
-        timeRemaining--;
-    } else {
-        timeEl.textContent = "Time is up";
-        clearInterval(countdown);
+// Get certain global elements
+document.getElementById("startButton").addEventListener("click",function() {
+    playQuiz();
+});
+document.getElementById("highscore-link").addEventListener("click", function() {
+    showHighScores();
+});
+
+// Function to start playing the quiz
+function playQuiz() {
+    document.getElementById("startButton").style.display = "none";
+
+
+    startClock();
+    showQuestion();
+}
+
+// Function called when View Highscores is clicked
+function showHighScores() {
+
+}
+
+// Show a question on the screen
+function showQuestion() {
+
+    // Get a random question from an imported array
+    var randomQuestion = questionArray[Math.floor(Math.random() * questionArray.length)];
+    
+    // Show that random question on screen
+    var questionEl = document.getElementById("description");
+    questionEl.textContent = randomQuestion.question;
+
+    // Add answers based on the question to the DOM
+    var ansEl = document.getElementById("choiceDescription");
+    ansEl.textContent = "";
+    for (var i = 0; i < randomQuestion.answers.length; i++) {
+        var ans = document.createElement("h6");
+        ans.textContent = randomQuestion.answers[i];
+        ansEl.appendChild(ans);
     }
-}, 1000);
 
+}
 
-
-
-setTimeout(function () {
-    var nextQuestion = document.createElement('h1');
-    nextQuestion.textContent = "This is also a question";
-    questionEl.textContent = "";
-    questionEl.appendChild(nextQuestion);
-
-}, 5000);
+// Timer function
+function startClock() {
+    var countdown = setInterval(function() {
+        if (timeRemaining > 1) {
+            timeEl.textContent = `${timeRemaining} seconds remaing`;
+            timeRemaining--;
+        } else if (timeRemaining === 1) {
+            timeEl.textContent = `${timeRemaining} seconds remaing`;
+            timeRemaining--;
+        } else {
+            timeEl.textContent = "Time is up";
+            clearInterval(countdown);
+        }
+    }, 1000);
+}
---
+++ assets/js/questions.js
@@ -0,0 +1,23 @@
+export const questionArray = [
+    {
+        question: "What is JavaScript?",
+        answers: ["A", "B", "C", "D"]
+    },
+    {
+        question: "How many hours are there in a day?",
+        answers: ["1", "2", "C", "D"]
+    },
+    {
+        question:  "Who are you?",
+        answers: [1, 2, 3, 4]
+    },
+
+
+
+
+
+
+
+
+
+];
