8fc9f3d39b32811758eb3cc471e982ae84508aa4
Will Berner <will.o.berner@gmail.com>
2021-06-22 23:59:01 -0400

    Added more quiz logic - created answer choices, added a correct index to question objects.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,24 @@
 import { questionArray } from "./questions.js"
 
+let score = 0;
+let timeRemaining = 0;
+
 var timeEl = document.querySelector(".timer")
-
-var timeRemaining = 10;
-
-// Get certain global elements
-document.getElementById("startButton").addEventListener("click",function() {
-    playQuiz();
-});
-document.getElementById("highscore-link").addEventListener("click", function() {
-    showHighScores();
-});
 
 // Function to start playing the quiz
 function playQuiz() {
     document.getElementById("startButton").style.display = "none";
 
-
-    startClock();
+    startClock(75);
     showQuestion();
 }
 
 // Function called when View Highscores is clicked
 function showHighScores() {
-
 }
 
 // Show a question on the screen
-function showQuestion() {
+function showQuestion() { 
 
     // Get a random question from an imported array
     var randomQuestion = questionArray[Math.floor(Math.random() * questionArray.length)];
@@ -39,22 +30,30 @@ function showQuestion() {
     // Add answers based on the question to the DOM
     var ansEl = document.getElementById("choiceDescription");
     ansEl.textContent = "";
-    for (var i = 0; i < randomQuestion.answers.length; i++) {
-        var ans = document.createElement("h6");
-        ans.textContent = randomQuestion.answers[i];
+
+    randomQuestion.answers.forEach((answer, index) => {
+        var ans = document.createElement("button");
+        ans.textContent = ((index + 1) + ": " + answer);
+        ans.setAttribute("style", "display: block; height: 40px;")
+        ans.setAttribute("index", index);
         ansEl.appendChild(ans);
-    }
+    })
+
+    var correctIndex = randomQuestion.correctAnsIndex;
+    setUpChoices(correctIndex);
 
 }
 
 // Timer function
-function startClock() {
+function startClock(duractionInSeconds) {
+
+timeRemaining = duractionInSeconds;
     var countdown = setInterval(function() {
         if (timeRemaining > 1) {
-            timeEl.textContent = `${timeRemaining} seconds remaing`;
+            updateTimer();
             timeRemaining--;
         } else if (timeRemaining === 1) {
-            timeEl.textContent = `${timeRemaining} seconds remaing`;
+            updateTimer();
             timeRemaining--;
         } else {
             timeEl.textContent = "Time is up";
@@ -62,3 +61,34 @@ function startClock() {
         }
     }, 1000);
 }
+
+function updateTimer() {
+    timeEl.textContent = `${timeRemaining} seconds remaing`;
+}
+
+// Sets up event listeners
+function setEventListeners() {
+    // Get certain global elements
+    document.getElementById("startButton").addEventListener("click",function() {
+        playQuiz();
+    });
+    document.getElementById("highscore-link").addEventListener("click", function() {
+        showHighScores();
+    });
+}
+
+function setUpChoices(correctAnsIndex) {
+    document.getElementById("choiceDescription").addEventListener("click", function(event) {
+        console.log(parseInt(event.target.getAttribute('index')));
+        console.log(correctAnsIndex);
+        if (parseInt(event.target.getAttribute('index')) === correctAnsIndex) {
+            score++;
+        } else {
+            score--;
+            timeRemaining = timeRemaining - 10;
+            updateTimer();
+        }
+    });
+}
+
+setEventListeners();
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,15 +1,27 @@
 export const questionArray = [
     {
         question: "What is JavaScript?",
-        answers: ["A", "B", "C", "D"]
+        answers: ["Pain", "Death", "Suffering", "Life"],
+        correctAnsIndex: 3
     },
     {
         question: "How many hours are there in a day?",
-        answers: ["1", "2", "C", "D"]
+        answers: ["24", "23", "22", "21"],
+        correctAnsIndex: 0
     },
     {
         question:  "Who are you?",
-        answers: [1, 2, 3, 4]
+        answers: ["Will", "Bobby", "Jimmy", "Gumbo"],
+        correctAnsIndex: 0
+
+    },
+    {
+        question:  "What is true of while loops?",
+        answers: ["A while-loop will always run longer than a for-loop.",
+         "While loops run faster than for-loops",
+         "While loops can be used if you don't know something's length",
+         "While loops cannot cause indexing errors"],
+         correctAnsIndex: 2
     },
 
 
